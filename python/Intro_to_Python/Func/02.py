5155a4dc8573bdf5cba30e16d92122c69ed1dde3
Shuchalin Georgiy <Shuchalin_Georgiy@live.ru>
2023-11-14 15:49:30 +0300

    directory python removed

--- python/Intro_to_Python/Func/02.py
+++
@@ -1,8 +0,0 @@
-import random
-
-len_list: int = 3
-
-rand_list = tuple(random.randint(-100, 100) for i in range(len_list))
-
-print(rand_list)
-print(max(rand_list))
--- python/Intro_to_Python/Modules and bibl/01.py
+++
@@ -1,7 +0,0 @@
-import modules_01.EditDir as EditDir
-import modules_01.RandChoise as RndChoice
-
-EditDir.new_dir('dir', 9)
-EditDir.del_dir('dir', 9)
-
-print(RndChoice.rand_elem_list([2, 7, 2, 9, 143]))
